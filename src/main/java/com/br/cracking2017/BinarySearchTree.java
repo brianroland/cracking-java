669f389606bd8c07b007190f01ccc7b7b7bb347c
Brian Roland <brian.roland.1@gmail.com>
2017-07-17 15:05:04 -0700

    cleanup

--- src/main/java/com/br/cracking2017/BinarySearchTree.java
+++ src/main/java/com/br/cracking2017/BinarySearchTree.java
@@ -1,13 +1,71 @@
 package main.java.com.br.cracking2017;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 /**
  * Created by brianroland on 7/17/17.
  *
  * build the collection of functions that constitute the interface to a Binary Search Tree
  *
  *
+ *  implemented: min, successor
+ *
+ *
  */
+
 public class BinarySearchTree {
+
+    public static NodeForBST max(NodeForBST root) throws Exception {
+        throw new NotImplementedException();
+    }
+
+    public static NodeForBST predecessor(NodeForBST node) throws Exception {
+        throw new NotImplementedException();
+    }
+
+    public static NodeForBST search(NodeForBST root, NodeForBST query) throws Exception {
+        throw new NotImplementedException();
+    }
+
+    public static NodeForBST insert(NodeForBST root, NodeForBST node) throws Exception {
+        throw new NotImplementedException();
+    }
+
+    public static NodeForBST delete(NodeForBST root, NodeForBST target) throws Exception {
+        throw new NotImplementedException();
+    }
+
+    public static NodeForBST min(NodeForBST root) throws Exception {
+
+        assert root!=null;
+        NodeForBST curr = root;
+        while (curr.left!=null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+
+    public static NodeForBST successor(NodeForBST node) throws Exception {
+        if ( isLeftChild(node)){
+
+            if (node.right!=null) {
+                return min(node.right);
+            } else {
+                return node.parent;
+            }
+
+        } else if (isRightChild(node)) {
+
+            if (node.right!=null) {
+                return min(node.right);
+            } else {
+                return firstRightParent(node);
+            }
+        }
+
+        //no parent
+        return node.right;
+    }
 
     public static boolean isLeftChild(NodeForBST node) {
 
@@ -31,16 +89,6 @@ public class BinarySearchTree {
         return false;
     }
 
-    public static NodeForBST min(NodeForBST root) throws Exception {
-
-        assert root!=null;
-        NodeForBST curr = root;
-        while (curr.left!=null) {
-            curr = curr.left;
-        }
-        return curr;
-    }
-
     public static NodeForBST firstRightParent(NodeForBST node) throws Exception {
 
         assert isRightChild(node);
@@ -54,29 +102,6 @@ public class BinarySearchTree {
                 return curr.parent;
             }
         }
-
         return null;    //never found a successor
-    }
-
-    public static NodeForBST successor(NodeForBST node) throws Exception {
-        if ( isLeftChild(node)){
-
-            if (node.right!=null) {
-                return min(node.right);
-            } else {
-                return node.parent;
-            }
-
-        } else if (isRightChild(node)) {
-
-            if (node.right!=null) {
-                return min(node.right);
-            } else {
-                return firstRightParent(node);
-            }
-        }
-
-        //no parent
-        return node.right;
     }
 }
